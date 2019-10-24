a8d71e885dc1f1b93c0c7a114df7c3c5b40994dd
Kevin Draven <kevindravenk@gmail.com>
2019-10-24 20:22:11 +0700

    [ADD] DB Connection
    untuk connect ke DB

---
+++ DBConnection.php
@@ -0,0 +1,52 @@
+<?php
+class DB{
+    public $dbconnection;
+
+    function __construct(){
+        $this->servername = "10.100.70.70\akreditasi2020";
+        $this->username = "guestManPro";
+        $this->password = "Testing123";
+        $this->dbname = "akreditasi2020";
+        $this->dbConInfo= array("Database"=>$this->dbname,"UID"=>$this->username,"PWD"=>$this->password);
+    }
+
+    function openConnection(){
+        $this->dbconnection = sqlsrv_connect($this->servername,$this->dbConInfo);
+        
+		if ($this->dbconnection) {
+			echo "Connection established.<br />";
+		} else {
+			echo "Connection could not be established.<br />";
+			die(print_r(sqlsrv_errors(), true));
+		}
+    }
+
+    function executeStoredProcedure($query, $param){
+        $this->openConnection();
+        $noCount = sqlsrv_query($this->dbconnection,"SET NOCOUNT ON");
+        $statement = sqlsrv_prepare($this->dbconnection,$query,$param);
+		$query_result = sqlsrv_execute($statement);
+		while ($row = sqlsrv_fetch_array($statement, SQLSRV_FETCH_NUMERIC)) {
+			if ( $row === false) {
+				if (($errors = sqlsrv_errors()) != null) {
+					foreach ($errors as $error) {
+						echo "SQLSTATE: " . $error['SQLSTATE'] . "<br />";
+						echo "code: " . $error['code'] . "<br />";
+						echo "message: " . $error['message'] . "<br />";
+					}
+				}
+			} 
+			$result[] = $row;
+		}
+		sqlsrv_close($this->db_connection);
+		return $result;
+	}
+}
+
+//caranya
+$db = new DB();
+$test->openConnection();
+$query = "EXEC Table3b1_RekognisiDTPS";
+$hasil = $test->executeStoredProcedure($query,[]);
+print_r($hasil);
+?>
