8f5123820021a348e6882ee6039195a6acbdea64
Ttanus <trashcanlimited@gmail.com>
2019-10-25 21:24:58 +0700

    layout upload
    layout sementara

---
+++ layout/layout.php
@@ -0,0 +1,65 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<link href="https://netdna.bootstrapcdn.com/bootstrap/3.2.0/css/bootstrap.min.css" rel="stylesheet" id="bootstrap-css">
+		<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+		<script src="https://netdna.bootstrapcdn.com/bootstrap/3.2.0/js/bootstrap.min.js"></script>
+		<link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.4.1/css/all.css" integrity="sha384-5sAR7xN1Nv6T6+dT2mhtzEpVJvfS3NScPQTrOxhwjIuvcA67KV2R5Jz6kr4abQsz" crossorigin="anonymous">
+		<link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css">
+		<link rel="stylesheet" href="https://www.w3schools.com/lib/w3-colors-flat.css">
+        <link rel="stylesheet" href="../style/style.css">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css">
+        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+		<title>
+			<!--insert title heie-->
+			<h1>Home</h1>
+		</title>
+
+	</head>
+	<body>
+		<div class="heading w3-flat-peter-river">
+			<div class="w3-cell w3-cell-middle w3-padding">
+                <div id="mySidenav" class="sidenav">
+                    <a href="javascript:void(0)" class="closebtn" onclick="closeNav()">&times;</a>
+                    <a href="home.html">Home</a>
+                    <a href="visidanmisi.html">Visi dan Misi</a>
+					<div class="w3-dropdown-hover">
+	                    <a href="TataPamong,Tata KeloladanKerja.html">
+	                    	Tata Pamong, Tata Kelola dan Kerja <i class="fa fa-caret-down"></i>
+	                    </a>
+	                    <div class="w3-dropdown-content w3-bar-block" style="margin-left: 7%;">
+							<a href="kerjasamatridharma(pendidikan).html" class="w3-bar-item w3-button">Kerjasama Tridharma(Pendidikan)</a>
+							<a href="kerjasamatridharma(penelitian).html" class="w3-bar-item w3-button">Kerjasama Tridharma(Penelitian)</a>
+							<a href="kerjasamatridharma(Pengabdian).html" class="w3-bar-item w3-button">Kerjasama Tridharma(Pengabdian kepada Masyarakat)</a>
+	                    </div>
+                  	</div> 
+					<div class="w3-dropdown-hover">
+						<a href="mahasiswa.html">Mahasiswa <i class="fa fa-caret-down"></i></a>
+						<div class="w3-dropdown-content w3-bar-block" style="margin-left: 17%;">
+							<a href="kualitasmahasiswa.html" class="w3-bar-item w3-button" style="width: 300%;">Kualitas Input Mahasiswa</a>
+							<a href="mahasiswaasing.html" class="w3-bar-item w3-button" style="width: 300%;">Mahasiswa Asing</a>
+						</div>
+					</div> 
+                        <a href="sdmahasiswa.html">Sumber Daya Mahasiswa</a>
+                        <a href="keuangansarana.html">Keuangan, Sarana, dan Prasarana</a>
+                        <a href="pendidikan.html">Pendidikan</a>
+                        <a href="penelitian.html">Penelitian</a>
+                        <a href="pkm.html">Pengabdian Kepada Masyarakat</a>
+                        <a href="lurahcapaian.html">Lurah dan Capaian Tridharma</a>
+                </div>
+                <span style="font-size:30px;cursor:pointer" onclick="openNav()">&#9776; </span>
+			</div>
+			<div class="w3-cell w3-cell-middle w3-padding">
+        		<img src="image/Logo-UNPAR.png" height="100px" width="100px">
+            </div>
+            <div class="w3-cell w3-cell-middle w3-padding">
+                <img src="image/LogoInformatika.jpg" height="100px" width="400px">
+            </div>
+            <div class="w3-cell w3-cell-middle w3-padding">
+                <p>Program Studi Informatika</p>
+                <p>Universitas Katolik Parahyangan</p>
+            </div>
+		</div>
+	</body>
+</html>
