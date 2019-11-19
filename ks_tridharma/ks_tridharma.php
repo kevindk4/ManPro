e5048f9c2895a94917c1aee589a79a96772a8787
williamo1099 <williamoktavianus1099@gmail.com>
2019-11-19 09:02:38 +0700

    Visualisasi pie chart

--- ks_tridharma/ks_tridharma.php
+++ ks_tridharma/ks_tridharma.php
@@ -113,6 +113,15 @@ require "../DBConnection.php"
             </tr>
 
             <?php
+            $dataPoints = array(
+                array("label" => "Chrome", "y" => 64.02),
+                array("label" => "Firefox", "y" => 12.55),
+                array("label" => "IE", "y" => 8.47),
+                array("label" => "Safari", "y" => 6.08),
+                array("label" => "Edge", "y" => 4.29),
+                array("label" => "Others", "y" => 4.59)
+            );
+
             $conn = new DB();
             $result = $conn->executeStoredProcedure("SELECT * FROM Kerjasama", []);
 
@@ -155,10 +164,29 @@ require "../DBConnection.php"
             ?>
         </table>
     </div>
+
+    <div id="chartContainer" style="height: 370px; width: 100%;"></div>
     <div class="footer">
         <p>Copyright <i class="fa fa-copyright"> 2019 by Tim Besar II Manpro</i></p>
     </div>
 </body>
-<script type="text/javascript" src="../script/script.js"></script>
+<script type="text/javascript" src="../script/script.js">
+    window.onload = function() {
+        var chart = new CanvasJS.Chart("chartContainer", {
+            animationEnabled: true,
+            title: {
+                text: "Usage Share of Desktop Browsers"
+            },
+            data: [{
+                type: "pie",
+                yValueFormatString: "#,##0.00\"%\"",
+                indexLabel: "{label} ({y})",
+                dataPoints: <?php echo json_encode($dataPoints, JSON_NUMERIC_CHECK); ?>
+            }]
+        });
+        chart.render();
+
+    }
+</script>
 
 </html>
